3872220b7de91f3f9b3f57d60927506ebe8923c1
Zhang Zhi <Zhang48240@gmail.com>
2021-05-18 12:14:20 -0400

    updateoffer

--- public/js/patientoffer.js
+++ public/js/patientoffer.js
@@ -18,15 +18,35 @@ $(function() {
     $("#decline").click(function() {
 
 
-        console.log("hah");
-        // window.location.href = "./offer.php"
+
+        console.log($(" #paid ").html());
+        $.post("../api/patientofferupdate.php", {
+                method: "accept",
+                paid: $(" #paid ").html(),
+                aid: $(" #aid ").html(),
+            },
+            function(r, err) {
+                console.log(r);
+                //window.location.href = "./offer.php"
+            }
+        );
 
     });
 
     $("#decline").click(function() {
 
-        console.log("hah");
-        // window.location.href = "./offer.php"
+
+        console.log($(" #paid ").html());
+        $.post("../api/patientofferupdate.php", {
+                method: "accept",
+                paid: $(" #paid ").html(),
+                aid: $(" #aid ").html(),
+            },
+            function(r, err) {
+                console.log(r);
+                //window.location.href = "./offer.php"
+            }
+        );
 
     });
 
