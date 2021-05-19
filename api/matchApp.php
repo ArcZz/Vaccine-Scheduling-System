e8a2b0a18a57b9e3c8dea16573d1ccc266d14c79
JennyZ <64447128+jz1433@users.noreply.github.com>
2021-05-18 23:18:27 -0400

    latest

--- api/matchApp.php
+++ api/matchApp.php
@@ -1,6 +1,9 @@
 <?php
 include 'db.php';
 
+// crontab -e
+// 0 12 * * * lynx -dump http://localhost/Vaccine-Scheduling-System/api/matchApp.php
+
 //calculate Distance
 function distance($lat1, $lon1, $lat2, $lon2) {
   if (($lat1 == $lat2) && ($lon1 == $lon2)) {
@@ -16,9 +19,6 @@ function distance($lat1, $lon1, $lat2, $lon2) {
     return $miles;
   }
 }
-
-// echo distance(32.9697, -96.80322, 29.46786, -98.53506) . " Miles<br>";
-
 
 // check and update any status before assign matching
 $status = "vaccinated";
@@ -48,11 +48,12 @@ echo 'total unassigned appointment: '.$appTotalCount. "<br>";
 echo '<pre>'; print_r($appArray); echo '</pre>';
 
 //get unassigned patients
-$unassignedPatient = $db->get_results('SELECT pa_id, p_number, max_travel_distance, pa_address
-                                  FROM Patient
-                                  WHERE pa_id NOT IN (SELECT pa_id
-					                                             FROM AppOffer
-					                                             WHERE status = "accepted" or status = "pending" or status = "vaccinated")');
+$unassignedPatient = $db->get_results('SELECT P.pa_id, p_number, max_travel_distance, pa_address, IFNULL(day_of_week, -1) AS day_of_week, IFNULL(starttime, "00:00:00") AS starttime, IFNULL(endtime, "00:00:00") AS endtime
+                                        FROM Patient P LEFT JOIN PatientPreferredSlot PS ON P.pa_id = PS.pa_id
+                                        LEFT JOIN Slot S on PS.sid = S.sid
+                                        WHERE P.pa_id NOT IN (SELECT pa_id
+                                        					FROM AppOffer
+                                        					WHERE status = "accepted" or status = "pending" or status = "vaccinated")');
 $paArrayGroup1 = array();
 $paArrayGroup2 = array();
 $paArrayGroup3 = array();
@@ -84,10 +85,20 @@ function createMatrix($paArrayGroup, $appArray)
       // echo "pa_lon: ".$pa_lon.'<br/>';
       // echo "pr_lat: ".$pr_lat.'<br/>';
       // echo "pr_lon: ".$pr_lon.'<br/>';
-       // echo "distance: ".$distance.'<br/>';
-       // echo "max distance: ".$paArrayGroup1[$i]->max_travel_distance.'<br/>';
+      echo '<br/>';
+      echo "paid: ".$paArrayGroup[$i]->pa_id.'<br/>';
+       echo "distance: ".$distance.'<br/>';
+       echo "max distance: ".$paArrayGroup[$i]->max_travel_distance.'<br/>';
+       $weeknum = date("w", strtotime($appArray[$j]->appdt));
+       $time = date("H:i:s", strtotime($appArray[$j]->appdt));
+       echo "week number: ".$weeknum."<br/>";
+       echo "preferred week number: ".$paArrayGroup[$i]->day_of_week."<br/>";
+       echo "app time: ".$time."<br/>";
+       echo "start time: ".$paArrayGroup[$i]->starttime."<br/>";
+       echo "end time: ".$paArrayGroup[$i]->endtime."<br/>";
 
-      if($distance <= $paArrayGroup[$i]->max_travel_distance){
+
+      if($distance <= $paArrayGroup[$i]->max_travel_distance && $weeknum == $paArrayGroup[$i]->day_of_week && $time <= $paArrayGroup[$i]->endtime &&  $time >= $paArrayGroup[$i]->starttime){
         array_push($valArray, 1);
       }else{
         array_push($valArray, 0);
@@ -101,7 +112,7 @@ function createMatrix($paArrayGroup, $appArray)
 
 $matrix1 = createMatrix($paArrayGroup1, $appArray);
 // test print matrix
- // echo '<pre>'; print_r($matrix1); echo '</pre>';
+ echo '<pre>'; print_r($matrix1); echo '</pre>';
 
 
 function bpm($bpGraph, $u, &$seen, &$matchR, $N)
@@ -145,10 +156,12 @@ echo '<pre>'; print_r($matching1); echo '</pre>';
 // add group 1 matchings to resultPairs
 // add matched appointment to array takenApp
 $resultPairs = array();  // array of [aid, pa_id, appdt]s
+$takenPatient = array();
 $takenApp = array();
 for ($i=0; $i < count($matching1); $i++) {
   // echo "val: ".$matching1[$i].'<br/>';
-  if($matching1[$i] != -1){
+  if($matching1[$i] != -1 && !in_array($paArrayGroup1[$matching1[$i]]->pa_id, $takenPatient)){
+    array_push($takenPatient, $paArrayGroup1[$matching1[$i]]->pa_id);
     array_push($resultPairs, array($appArray[$i]->aid, $paArrayGroup1[$matching1[$i]]->pa_id, $appArray[$i]->deadlinedt));
     array_push($takenApp, $i);
   }
@@ -173,14 +186,15 @@ for ($i=0; $i < count($appArray); $i++) {
 // get matchings for Group 2
 $matrix2 = createMatrix($paArrayGroup2, $appArray2);
 $matching2 = maxBPM($matrix2, count($appArray2), count($paArrayGroup2));
-// echo '<pre>'; print_r($matching2); echo '</pre>';
+echo '<pre>'; print_r($matching2); echo '</pre>';
 
 // add group 2 matchings to resultPairs
 // add matched appointment to array takenApp2
 $takenApp2 = array();
 for ($i=0; $i < count($matching2); $i++) {
   // echo "val: ".$matching2[$i].'<br/>';
-  if($matching2[$i] != -1){
+  if($matching2[$i] != -1 && !in_array($paArrayGroup2[$matching2[$i]]->pa_id, $takenPatient)){
+    array_push($takenPatient, $paArrayGroup2[$matching2[$i]]->pa_id);
     array_push($resultPairs, array($appArray2[$i]->aid, $paArrayGroup2[$matching2[$i]]->pa_id, $appArray[$i]->deadlinedt));
     array_push($takenApp2, $i);
   }
@@ -202,19 +216,19 @@ for ($i=0; $i < count($appArray2); $i++) {
 // echo 'total unassigned appointment after group1 done: '.count($appArray3). "<br>";
 // echo '<pre>'; print_r($appArray3); echo '</pre>';
 
-// get matchings for Group 2
+// get matchings for Group 3
 $matrix3 = createMatrix($paArrayGroup3, $appArray3);
 $matching3 = maxBPM($matrix3, count($appArray3), count($paArrayGroup3));
-// echo '<pre>'; print_r($matching3); echo '</pre>';
+ echo '<pre>'; print_r($matching3); echo '</pre>';
 
 // add group 3 matchings to resultPairs
 for ($i=0; $i < count($matching3); $i++) {
   // echo "val: ".$matching3[$i].'<br/>';
-  if($matching3[$i] != -1){
+  if($matching3[$i] != -1 && !in_array($paArrayGroup3[$matching3[$i]]->pa_id, $takenPatient)){
     array_push($resultPairs, array($appArray3[$i]->aid, $paArrayGroup3[$matching3[$i]]->pa_id, $appArray[$i]->deadlinedt));
   }
 }
-// echo '<pre>'; print_r($resultPairs); echo '</pre>';
+ echo '<pre>'; print_r($resultPairs); echo '</pre>';
 
 
 //add new matchings: update AppOffer table
@@ -237,7 +251,7 @@ foreach ($resultPairs as $pair) {
   if(!isset($result[0]->pa_id)){
     // if not exist in DB, then add to DB
     $db->query_prepared('INSERT INTO AppOffer(pa_id, aid, offerdt, deadlinedt,status)
-                        VALUES(?,?,?,?,?);',
+                        VALUES(?,?,?,?,?)',
                         [$pa_id, $aid, $now, $deadline, $status]);
   }else{
     $db->query_prepared('Update AppOffer SET status= "pending", replydt = NULL, offerdt =?, deadlinedt =? WHERE pa_id = ? AND aid = ?',
